b871f1094310af5efa3eebf5f96da2e7dc566fa4
amandamb <manda.m.bell@gmail.com>
2012-05-08 19:00:33 -0400

    added airplane enemies

--- main.py
+++ main.py
@@ -96,8 +96,51 @@ class Balloon (pygame.sprite.Sprite):
         '''draw the balloon in its current position'''
         if self.active:
             self.screen.blit(self.image, (self.x, self.y))
+            
+            
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, screen, init_x, init_y, dx, dy, image_file):
+        '''initialize the enemy sprite'''
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.screen = screen
+        self.x = init_x
+        self.y = init_y
+        if self.x >= SCREEN_WIDTH / 2:
+            dx = dx * -1
+        self.dx = dx
+        self.dy = dy
+        
+        self.image = pygame.transform.scale (load_image(image_file), (100, 50))
+        self.rect = self.image.get_rect()
+        self.rect.move(self.x, self.y)
+        self.rect.topleft = (self.x, self.y)
+        self.image_w, self.image_h = self.image.get_size()
+        self.rect.bottomright = (self.x + self.image_w, self.y + self.image_h)
+        
+        self.active = True
+        
+        
+    def update(self):
+        '''move and update the sprite'''
+        #if (self.x < 0) or (self.x > SCREEN_WIDTH) or (self.y <0) or (self.y > SCREEN_HEIGHT):
+        #   self.active = False
+        self.x += self.dx
+        self.y += self.dy
+        self.rect.move(self.x, self.y)
+        self.rect.topleft = (self.x, self.y)
+        self.rect.bottomright = (self.x + self.image_w, self.y + self.image_h)
+        
+    
+    def draw(self):
+        '''if the enemy is active, draw it to the screen'''
+        if self.active:
+            self.screen.blit(self.image, (self.x, self.y))
+     
+    
     
 def main():
+    '''main function that runs the game'''
     pygame.init()
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Sky High')
@@ -129,17 +172,21 @@ def main():
         android.accelerometer_enable(True)
 
     # Use a timer to control FPS.
-    pygame.time.set_timer(TIMEREVENT, 1000 / FPS)
+    pygame.time.set_timer(TIMEREVENT, 3000)
     pygame.time.set_timer(USEREVENT + 1, 1000)
+    pygame.time.set_timer(USEREVENT + 2, 2000)
     
     pygame.key.set_repeat(FPS, FPS) # set key repeat on 
     
     balloon = Balloon(screen, SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT, 0, balloon_speed, "assets/balloon.gif")
     
+    enemies = []
+    
     while True:
+        #game loop
         time_passed = clock.tick(FPS)
         
-        text = font.render("Score: " + str(score), 1, (0, 0, 0))
+        text = font.render("Score: " + str(score), 1, (0, 0, 0)) #render score
         
         if android:
             balloon_move = android.accelerometer_reading()
@@ -153,6 +200,12 @@ def main():
 
             if android.check_pause():
                 android.wait_for_resume()
+                
+        init_x = randint(0, SCREEN_WIDTH)
+        if init_x < SCREEN_WIDTH/2:
+            enemy_image = "assets/plane-right.gif"
+        else:
+            enemy_image = "assets/plane-left.gif"
         
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -167,18 +220,34 @@ def main():
                         balloon.x -= 5.0
             elif event.type == USEREVENT + 1:
                 score += 1
-        
+            elif event.type == TIMEREVENT and score>=2 and score<=10:
+                enemies.append(Enemy(screen, init_x, -50, randint(0, 3), randint(0, 3), enemy_image)) 
+            
+            elif event.type == USEREVENT + 2 and score>=10: 
+                enemies.append(Enemy(screen, init_x, -50, randint(1, 5), randint(1, 5), enemy_image)) 
+
+                
+                
         screen.fill(background)
         
         sky.update()
         sky.draw()
-
         
         balloon.update()
         balloon.draw()
         if balloon.y <= SCREEN_HEIGHT / 3:
             balloon.dy = 0
             sky.scrolling = True
+            
+        for enemy in enemies:
+            if pygame.sprite.collide_mask(enemy, balloon):
+                # ADD GAME OVER SCREEN HERE
+                pygame.quit()
+                sys.exit()
+            enemy.update()
+            enemy.draw()
+            
+            
             
         screen.blit(text, (0,  SCREEN_HEIGHT - 30))
         pygame.display.flip()
